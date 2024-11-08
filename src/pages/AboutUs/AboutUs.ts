5592f67c026ecffc54042e78b1273903b50d3c4e
dh <goleiden@gmail.com>
2024-11-09 01:03:22 +0200

    added about us

--- src/pages/AboutUs/AboutUs.ts
+++ src/pages/AboutUs/AboutUs.ts
@@ -1,15 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {Header} from '../../components/Header';
 import {WIP} from '../../components/WIP';
 import {Page} from '../../components/Page';
+import {Logo} from '../../components/Logo';
+import {ThemeService} from '../../app/theme.service';
 
 @Component({
   selector: 'about-us',
   standalone: true,
-  imports: [RouterOutlet, Header, WIP, Page],
+  imports: [RouterOutlet, Header, WIP, Page, Logo],
   templateUrl: './AboutUs.html',
   styleUrls: ['./AboutUs.scss']
 })
-export class AboutUs {
+export class AboutUs implements OnInit  {
+  constructor(protected themeService: ThemeService) {
+  }
+
+  ngOnInit() {
+    const savedTheme = this.themeService.getTheme();
+    this.themeService.applyTheme(savedTheme);
+  }
 }
