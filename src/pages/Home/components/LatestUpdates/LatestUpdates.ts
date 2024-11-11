1d9ed5702e914e50d8820815bc9c5ce07c40215f
dh <goleiden@gmail.com>
2024-11-11 17:08:46 +0200

    more adaptive UI

--- src/pages/Home/components/LatestUpdates/LatestUpdates.ts
+++ src/pages/Home/components/LatestUpdates/LatestUpdates.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {WIP} from '../../../../components/WIP';
-import {NgForOf, NgIf, SlicePipe} from '@angular/common';
+import {NgClass, NgForOf, NgIf, SlicePipe} from '@angular/common';
 import {ArrowIcon} from '../../../../assets/icons/Arrow';
 import {ToriiIcon} from '../../../../assets/icons/Torii';
 import {Router} from '@angular/router';
@@ -9,7 +9,7 @@ import {ThemeService} from '../../../../app/theme.service';
 @Component({
   selector: 'LatestUpdates',
   standalone: true,
-  imports: [WIP, NgIf, ArrowIcon, ToriiIcon, NgForOf, SlicePipe],
+  imports: [WIP, NgIf, ArrowIcon, ToriiIcon, NgForOf, SlicePipe, NgClass],
   templateUrl: './LatestUpdates.html',
   styleUrls: ['./LatestUpdates.scss']
 })
@@ -25,11 +25,11 @@ export class LatestUpdates {
   }
 
   onMouseEnter(index: number) {
-    this.isHovered[index] = true; // Задаємо true для відповідного індексу
+    this.isHovered[index] = true;
   }
 
   onMouseLeave(index: number) {
-    this.isHovered[index] = false; // Задаємо false для відповідного індексу
+    this.isHovered[index] = false;
   }
 
   goToManga(id: number) {
